841594bf1ae58c662b8fa5dd8986e90f90b0fa39
김승주 <eogks999@naver.com>
2024-04-04 06:51:31 +0900

    8

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
