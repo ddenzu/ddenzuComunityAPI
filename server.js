845492ad8016d48b5ec6ba8f93b349002f4a6e7b
김승주 <eogks999@naver.com>
2024-04-04 05:51:49 +0900

    승뮤니티 API 문서화

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/swagger');
+});
+
+app.get('*', (req, res) => {
+    res.redirect('/swagger');
+});
