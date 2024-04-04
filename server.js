79c9b9d9e2ff680381fdadc5d341761ffde46c67
김승주 <eogks999@naver.com>
2024-04-04 17:12:56 +0900

    17

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const http = require('http');
+const app = express()
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors({
+    origin: "http://www.ddenzu.store",
+    credentials: true,
+}));
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/swagger');
+});
+
+app.get('*', (req, res) => {
+    res.redirect('/swagger');
+});
